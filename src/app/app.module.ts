152778b4f6be60bfc1db133cf539f4db080a82f3
Mohammed Amine Grid <65062069+mohammed-amine-grid@users.noreply.github.com>
2021-03-11 15:55:05 +0100

    finished header for landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomepageComponent } from './homepage/homepage.component'
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './homepage/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
