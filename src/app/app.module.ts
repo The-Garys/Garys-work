6d1dd3844ec560f62dbc98740efb5f316ef0ee84
ramziChammem <ramzichammem69@gmail.com>
2021-03-11 17:54:52 +0100

    sign up and sign in forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignInComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SignInComponent, SignUpComponent],
+  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
