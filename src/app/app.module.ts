bef314afe89067fcedf89abfd0a64a1a9fb35d3f
ramziChammem <ramzichammem69@gmail.com>
2021-03-12 00:35:32 +0100

    saved the input values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, SignInComponent, SignUpComponent],
-  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  userData: {
+    firstName: any;
+    lastName: any;
+    email: any;
+    password: any;
+    rePassword: any;
+    phone: any;
+    gender: any;
+    profession: any;
+  };
+  getVal(val) {
+    console.log(val);
   }
-
+  getData(
+    firstName,
+    lastName,
+    email,
+    password,
+    rePassword,
+    phone,
+    gender,
+    profession
+  ) {
+    this.http
+      .post(
+        'http://localhost:3000/halim',
+        {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password,
+          rePassword: rePassword,
+          phone: phone,
+          gender: gender,
+          profession: profession,
+        },
+        { responseType: 'json' }
+      )
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
