5c93ed7c1d16f04f12f4420612008e78550c29eb
Yassmine Romdhane <yassmineromdhane1996@gmail.com>
2021-03-12 19:55:38 +0100

    filtered by name,professionAndLocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { ServicesListComponent } from './services-list/services-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MDBBootstrapModule.forRoot()
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services-list/mock-service.ts
@@ -0,0 +1,30 @@
+// import {service} from '../services-list/services-list.component'
+
+export const SERVICES= [
+
+    { id: 12,firstName : 'Ramzi', profession:'Painting', location:'Tunis',image: 'https://mywallworks.com/wp-content/uploads/2018/08/hiring-a-painter.jpeg' },
+    { id: 13,firstName : 'yassmine', profession:'HouseCleaning', location:'Gabes',image: 'https://thumbs.dreamstime.com/b/maide-faisant-le-lit-dans-l-h%C3%B4tel-130816667.jpg'},
+    { id: 14,firstName : 'Iness', profession:'Gardening', location:'Bizerte' ,image: 'https://www.yourfreecareertest.com/wp-content/uploads/2017/10/nursary_worker.jpg'},
+    { id: 15,firstName : 'Alaa', profession:'Installs',  location:'Djerba', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQREPWO9MLfdBmugt0llMR-t6b2IWdQhbiTIQ&usqp=CAU'},
+    { id: 16,firstName : 'MALEK', profession:'painting', location:'Sousse', image: 'https://mywallworks.com/wp-content/uploads/2018/08/hiring-a-painter.jpeg'},
+    { id: 17,firstName : 'Nabil', profession:'Repairing Doors',  location:'Keirwan',image: 'https://www.tashmans.com/images/repair-installation/door-repair-and-installation-los-angeles.jpg'},
+    { id: 18,firstName : 'Haythem', profession:'Repairing windows', location:'tuzeur', image: 'https://www.thespruce.com/thmb/M8MzPqr-Z6pMoHt8caLenAUqI9k=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/quality-light--construction-workers-installing-new-window-in-house-1129117294-327361dbc0f045efafc1dd2bfe5de070.jpg'},
+    { id: 19,firstName : 'Achref', profession:'Electricity', location:'Nabeul', image: 'https://www.obat.fr/blog/wp-content/uploads/2020/06/article_metier_electricien-1280x720.jpg'},
+    { id: 20,firstName : 'Grid', profession:'Gardening', location:'Hammamet', image: 'https://www.tashmans.com/images/repair-installation/door-repair-and-installation-los-angeles.jpg'},
+  ];
+
+
+  export const NAME = [
+
+    {  profession:'Painting', location:'Tunis',image: 'https://mywallworks.com/wp-content/uploads/2018/08/hiring-a-painter.jpeg' },
+    {  profession:'HouseCleaning', location:'Gabes',image: 'https://thumbs.dreamstime.com/b/maide-faisant-le-lit-dans-l-h%C3%B4tel-130816667.jpg'},
+    {  profession:'Gardening', location:'Bizerte' ,image: 'https://www.yourfreecareertest.com/wp-content/uploads/2017/10/nursary_worker.jpg'},
+    { profession:'Installs',  location:'Djerba', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQREPWO9MLfdBmugt0llMR-t6b2IWdQhbiTIQ&usqp=CAU'},
+    { profession:'painting', location:'Sousse', image: 'https://mywallworks.com/wp-content/uploads/2018/08/hiring-a-painter.jpeg'},
+    {  profession:'Repairing Doors',  location:'Keirwan',image: 'https://www.tashmans.com/images/repair-installation/door-repair-and-installation-los-angeles.jpg'},
+    { profession:'Repairing windows', location:'tuzeur', image: 'https://www.thespruce.com/thmb/M8MzPqr-Z6pMoHt8caLenAUqI9k=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/quality-light--construction-workers-installing-new-window-in-house-1129117294-327361dbc0f045efafc1dd2bfe5de070.jpg'},
+    {  profession:'Electricity', location:'Nabeul', image: 'https://www.obat.fr/blog/wp-content/uploads/2020/06/article_metier_electricien-1280x720.jpg'},
+    { profession:'Gardening', location:'Hammamet', image: 'https://www.tashmans.com/images/repair-installation/door-repair-and-installation-los-angeles.jpg'},
+  ];
+
+
