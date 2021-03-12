f1e7f84e65efb95c6fd1234daca26c6c4d5d4b04
ramziChammem <ramzichammem69@gmail.com>
2021-03-12 18:49:50 +0100

    fixed conflict in main

--- src/app/homepage/footer/footer.component.ts
+++ src/app/homepage/footer/footer.component.ts
@@ -1,19 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { HttpClient } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-form: FormGroup;
-  constructor(public fb: FormBuilder,
-    private http: HttpClient) { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
