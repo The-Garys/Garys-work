0515909979ff8b4088618bdba0d985c628077628
Mohammed Amine Grid <65062069+mohammed-amine-grid@users.noreply.github.com>
2021-03-15 16:00:17 +0100

    created nav rendered messages

---
+++ src/app/Admin/admin-messages.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminMessagesService {
+  apiURL: string = 'http://localhost:3000/api/contactus';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getMessages(){
+    return this.httpClient.get(this.apiURL);
+  }
+  
+}
