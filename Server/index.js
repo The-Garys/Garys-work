2171821eb0cac5e25a2805ea1ef6737bd89dae99
ramziChammem <ramzichammem69@gmail.com>
2021-03-16 11:27:39 +0100

    deleted npm cash

--- Server/index.js
+++ Server/index.js
@@ -6,16 +6,16 @@ var bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/UserRoute");
 const serviceProviderRouter = require("./routes/ServiceProviderRouter");
-const ServicesProviderListRouter = require("./routes/ServicesProviderListRoute")
+const ServicesProviderListRouter = require("./routes/ServicesProviderListRoute");
 const ContactUsRouter = require("./routes/ContactUsRoute");
-const reviewRouter= require("./routes/ReviewRoute");
-const appointmentRouter= require("./routes/AppointmentRoute")
+const reviewRouter = require("./routes/ReviewRoute");
+const appointmentRouter = require("./routes/AppointmentRoute");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
-const uri = process.env.URI ;
+const uri = process.env.URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -30,9 +30,8 @@ app.use("/api/contactus", ContactUsRouter);
 app.use("/api/serviceProvider", serviceProviderRouter);
 
 app.use("/api/serviceProviderList", ServicesProviderListRouter);
-app.use("/api/review", reviewRouter)
-app.use("/api/appointment", appointmentRouter)
-
+app.use("/api/review", reviewRouter);
+app.use("/api/appointment", appointmentRouter);
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
