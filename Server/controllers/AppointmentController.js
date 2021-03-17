e44a7de7c7e1068adf7620cdfd9f031a62817648
Yassmine Romdhane <yassmineromdhane1996@gmail.com>
2021-03-17 19:55:05 +0100

    add appointment

--- Server/controllers/AppointmentController.js
+++ Server/controllers/AppointmentController.js
@@ -4,11 +4,17 @@ module.exports={
     makeAppointment: async (req, res) => {
        const appointment = new Appointment({
            date:req.body.date,
+           time:req.body.time,
            userName: req.body.userName,
            email:req.body.email,
            serviceProviderName: req.body.serviceProviderName
        });
           try{
+          const check= await Appointment.find({date:req.body.date, time: req.body.time})
+          console.log('date and time', check)
+          if(check.length){
+            return res.send({data : "not avail"})
+          }
           const saveAppointment = await appointment.save();
           res.json(saveAppointment);
          } catch(err) {
@@ -16,12 +22,17 @@ module.exports={
          }
         },
     
-    getAll: async (req, res) => {
+      
+      getAll: async (req, res) => {
         try {
-          const appointment = await Appointment.find();
-          console.log(appointment);
+          const appointment = await Appointment.find({serviceProviderName : req.params.name});
+          console.log(req.params);
           res.send(appointment)
         } catch (error) {
           console.log(error);
         }
-      },}
+      },
+
+
+    
+    }
--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -10,11 +10,17 @@ import Swal from 'sweetalert2';
 export class AppointmentComponent implements OnInit {
 
   constructor(private http:HttpClient) { }
-
+data : any 
   ngOnInit(): void {
+    this.http.get("http://localhost:3000/api/appointment/malek").subscribe((data)=>{
+      console.log(data)
+      this.data = data
+      })
+
+      
   }
-  submit(name , email , date , svname ){ 
-    if(!name || !email || !date || !svname) {
+  submit(name , email , date , svname , time ){ 
+    if(!name || !email || !date || !svname ||!time) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
@@ -23,18 +29,25 @@ export class AppointmentComponent implements OnInit {
       })
     } else {
       this.http.post("http://localhost:3000/api/appointment",
-      {userName : name , email : email , date : date , serviceProviderName : svname}).subscribe((data)=>{
-       Swal.fire({
-         icon: 'success',
-         title: 'Appointment added successfully',
-         showConfirmButton: false,
-         timer: 1500
+      {userName : name , email : email , date : date , serviceProviderName:svname , time:time }).subscribe((data)=>{
+        console.log(data)
+        if(data["data"]){
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Not available!',
+            footer: '<a href>Why do I have this issue?</a>'
+          })
+        } else {
+          Swal.fire({
+            icon: 'success',
+            title: 'Appointment added successfully',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        }
+      
        })
-       })
-   
-   
-      console.log({name : name , email : email , date : date , svname : svname})
-    }
-  
- }
+       }
+    } 
 }
