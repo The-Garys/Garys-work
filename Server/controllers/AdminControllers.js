1bd2eeb9cdeeaec2fa3afde418efbd5fe666be68
AliGhanmi <ghanmiali7@gmail.com>
2021-03-18 19:54:42 +0100

    fix conflict

--- Server/controllers/AdminControllers.js
+++ Server/controllers/AdminControllers.js
@@ -1,20 +1,6 @@
 const Login = require("../models/AdminCred");
 
 const admin = {
-  post: async (req, res) => {
-    try {
-      let { email, password } = req.body;
-      const post = new Login({
-        email,
-        password,
-      });
-      await post.save();
-      res.send("account created");
-    } catch (err) {
-      res.send(err);
-    }
-  },
-
   verifyLogin: async (req, res) => {
     try {
       //   let { email, password } = req.body;
