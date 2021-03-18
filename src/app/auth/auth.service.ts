0e7dc3de6a8da278a695e275d7b280094e76b186
Mohammed Amine Grid <65062069+mohammed-amine-grid@users.noreply.github.com>
2021-03-18 15:09:05 +0100

    working on  route guarding

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    constructor() { }
+
+    admin : boolean = false;
+
+    isAuthenticated() {
+      return this.admin;
+    }
+}
