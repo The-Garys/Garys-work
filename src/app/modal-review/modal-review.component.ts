138b126c7465c5c2555b2fab71cff37a01c7e977
ramziChammem <ramzichammem69@gmail.com>
2021-03-21 17:45:04 +0100

    modal review's front end almost done

--- src/app/modal-review/modal-review.component.ts
+++ src/app/modal-review/modal-review.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
+import { GaryService } from '../gary.service';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-modal-review',
   templateUrl: './modal-review.component.html',
   styleUrls: ['./modal-review.component.scss'],
 })
 export class ModalReviewComponent implements OnInit {
-  constructor(config: NgbRatingConfig) {
+  constructor(
+    config: NgbRatingConfig,
+    private http: HttpClient,
+    private GaryService: GaryService
+  ) {
     config.max = 5;
     config.readonly = false;
   }
+  currentRate: number = 0;
   myReview = {
-    id: '',
+    rate: this.currentRate,
+    userId: 'idUser',
+    spId: 'idSp',
     title: '',
     body: '',
   };
-  currentRate: number = 0;
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  changeRate() {
+    this.myReview.rate = this.currentRate;
     console.log(this.myReview);
   }
-  sendReview() {}
+  addReview() {
+    console.log('checking review', this.myReview);
+  }
 }
