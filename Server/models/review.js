98b8be2582babf912ed40922c9c9e90421c322f3
ramziChammem <ramzichammem69@gmail.com>
2021-03-21 16:38:13 +0100

    merging

--- Server/models/review.js
+++ Server/models/review.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-const reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
     serviceProviderName: { type: String, required: true },
     userName: { type: String, required: true },
-    review: { type: String, required: true }
-}, { timestamps: true }
-)
+    rate: { type: Number, required: true },
+    review: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Reviews = mongoose.model("Reviews", reviewSchema);
 
