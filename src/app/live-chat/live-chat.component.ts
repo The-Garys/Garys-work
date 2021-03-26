e53cf56b103bace8c76114117a8c2dc45996c832
ramziChammem <ramzichammem69@gmail.com>
2021-03-26 15:57:29 +0100

    working on live messages

--- src/app/live-chat/live-chat.component.ts
+++ src/app/live-chat/live-chat.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { io } from 'socket.io-client';
 const SOCKET_ENDPOINT = 'localhost:3000';
-
 @Component({
   selector: 'app-live-chat',
   templateUrl: './live-chat.component.html',
@@ -19,24 +18,10 @@ export class LiveChatComponent implements OnInit {
     this.socket = io(SOCKET_ENDPOINT);
     this.socket.on('message-broadcast', (data: string) => {
       if (data) {
-        const element = document.createElement('li');
-        element.innerHTML = data;
-        element.style.background = 'white';
-        element.style.padding = '15px 30px';
-        element.style.margin = '10px';
-        document.getElementById('message-list').appendChild(element);
       }
     });
   }
   SendMessage() {
     this.socket.emit('message', this.message);
-    const element = document.createElement('li');
-    element.innerHTML = this.message;
-    element.style.background = 'white';
-    element.style.padding = '15px 30px';
-    element.style.margin = '10px';
-    element.style.textAlign = 'right';
-    document.getElementById('message-list').appendChild(element);
-    this.message = '';
   }
 }
