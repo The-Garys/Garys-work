6c4247c932f32d1f0ca327898331b12daefe3d58
Mohammed Amine Grid <65062069+mohammed-amine-grid@users.noreply.github.com>
2021-04-01 11:59:54 +0100

    sers list loc

--- src/app/user-services/user-services.component.ts
+++ src/app/user-services/user-services.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { SERVICES, NAME } from '../services-list/mock-service';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import {  NAME } from '../services-list/mock-service';
 import { HttpClient } from '@angular/common/http';
 import { LocalService } from '../local.service';
 import { Router } from '@angular/router';
 import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ServicesService } from '../services/services.service';
+import { MapsAPILoader } from '@agm/core';
+
 
 @Component({
   selector: 'app-user-services',
@@ -13,6 +15,14 @@ import { ServicesService } from '../services/services.service';
   providers: [NgbRatingConfig],
 })
 export class UserServicesComponent implements OnInit {
+  
+  map:boolean = false;
+  inp : string;
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder;
   services: any = [];
   username: string;
   list: any = NAME;
@@ -23,18 +33,48 @@ export class UserServicesComponent implements OnInit {
   n: any = ""
   l: any = ""
   p: any = this.local.pick
+
+  @ViewChild('search')
+  public searchElementRef: ElementRef;
+
   constructor(
     private http: HttpClient,
     private local: LocalService,
     private serviceList: ServicesService,
     private router: Router,
     config: NgbRatingConfig,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
   ) {
     config.max = 5;
     config.readonly = true;
   }
   role: string = this.local.role;
   ngOnInit(): void {
+
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.getAddress(this.latitude, this.longitude);
+          this.zoom = 12;
+        });
+      });
+    })
     
     console.log('dddddzsssadad', this.local.pick);
     this.list = NAME;
@@ -45,6 +85,67 @@ export class UserServicesComponent implements OnInit {
     this.getRating();
     
   }
+
+  // Get Current Location Coordinates
+  public setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+
+
+  markerDragEnd($event) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  onInputChange() {
+    console.log(this.inp);
+    
+  }
+
+
+  onChooseloc(event) {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.getAddress(this.latitude, this.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+
+
+   onLocChange(event) {
+     this.latitude = event.coords.lat;
+     this.longitude = event.coords.lng;
+     this.getAddress(this.latitude, this.longitude);
+   }
+
+
+
+
   getServices() {
 
     this.serviceList.getServiceProviders().subscribe((data) => {
@@ -138,18 +239,29 @@ export class UserServicesComponent implements OnInit {
     }
 
   }
+   
+
+  viewMap() {
+this.map = true;
+  }
 
   dropLoc(val) {
+    
     console.log(val);
     this.l = val.toUpperCase();
     this.services = this.backup;
     var newArray = [];
+    
     this.services.map((e) => {
       val = val.toUpperCase();
+      let reg = new RegExp(val, "s")
       var name = e.fullName.toUpperCase();
       var profession = e.profession.toUpperCase();
       var location = e.location.toUpperCase();
-      if (name.includes(this.n) && profession.includes(this.p) && location.includes(val)) {
+      
+
+
+      if (name.includes(this.n) && profession.includes(this.p) && reg.test(location)) {
         newArray.push(e);
       }
     });
