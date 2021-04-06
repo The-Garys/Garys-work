932d26c0010ed8ce5e94a6ec0829842ebf7e1973
ramziChammem <ramzichammem69@gmail.com>
2021-04-06 17:50:59 +0100

    refactoring sign-in sign-up

--- src/app/newsignup/newsignup.component.ts
+++ src/app/newsignup/newsignup.component.ts
@@ -3,28 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newsignup',
   templateUrl: './newsignup.component.html',
-  styleUrls: ['./newsignup.component.scss']
+  styleUrls: ['./newsignup.component.scss'],
 })
 export class NewsignupComponent implements OnInit {
+  constructor() {}
+  spSignUp: boolean = false;
+  userSignup: boolean = false;
+  container: any;
+  ngOnInit(): void {}
 
-  constructor() { }
-
-   container : any   
-  ngOnInit(): void {
-
-    
-
-    
+  signupSp() {
+    this.spSignUp = true;
+    document.querySelector('.container').classList.add('sign-up-mode');
+  }
+  signupUser() {
+    this.userSignup = true;
+    document.querySelector('.container').classList.add('sign-up-mode');
   }
 
-       signup(){
-        document.querySelector(".container").classList.add("sign-up-mode");
-
-       }
-   
-    
-    signin(){
-      document.querySelector(".container").classList.remove("sign-up-mode");
-    }
-  
+  signin() {
+    this.userSignup = false;
+    this.spSignUp = false;
+    document.querySelector('.container').classList.remove('sign-up-mode');
+  }
 }
